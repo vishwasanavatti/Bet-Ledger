28c01ffcc25027c6b02329b73bd406f4721d7e26
vishwasanavatti <vishwasanavatti@gmail.com>
2020-09-07 12:54:56 +0200

    added search functionality

--- src/app/home/fixture/fixture.page.ts
+++ src/app/home/fixture/fixture.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-fixture',
@@ -9,12 +10,15 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FixturePage implements OnInit {
   fixtures: any;
+  searchTerm: string;
+  filterItems: any;
 
   constructor(private navController: NavController, private http: HttpClient) {}
 
   ngOnInit() {
     const json = require('../../../assets/fixtures.json');
     this.fixtures = json.fixtures;
+    this.filterItems = this.fixtures;
     /*this.http
       .get(
         'https://cors-anywhere.herokuapp.com/https://cricapi.com/api/matches/?apikey=yJMDk1hYBZYZ92CVYtQzJb65oRq1'
@@ -22,6 +26,25 @@ export class FixturePage implements OnInit {
       .subscribe((response) => {
         console.log(response);
       });*/
+  }
+
+  filter() {
+    if (this.searchTerm && this.searchTerm.trim() !== ' ') {
+      this.filterItems = this.fixtures.filter(
+        (item) =>
+          item.team1
+            .toLowerCase()
+            .indexOf(this.searchTerm.toLowerCase().trim()) > -1 ||
+          item.team2
+            .toLowerCase()
+            .indexOf(this.searchTerm.toLowerCase().trim()) > -1 ||
+          item.date
+            .toLowerCase()
+            .indexOf(this.searchTerm.toLowerCase().trim()) > -1
+      );
+    } else {
+      this.filterItems = this.fixtures;
+    }
   }
 
   check() {
