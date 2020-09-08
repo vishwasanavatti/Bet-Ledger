701c283aaea889c4d6e1b4fb8ded9e9ec95edda4
vishwasanavatti <vishwasanavatti@gmail.com>
2020-09-08 23:55:43 +0200

    created a component for bet update

---
+++ src/app/home/update-bet/update-bet-entry/update-bet-entry.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-bet-entry',
+  templateUrl: './update-bet-entry.component.html',
+  styleUrls: ['./update-bet-entry.component.scss'],
+})
+export class UpdateBetEntryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
