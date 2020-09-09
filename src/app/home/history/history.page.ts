a6eb63b5e897354a520de8550f19058eda415a89
vishwasanavatti <vishwasanavatti@gmail.com>
2020-09-09 02:18:18 +0200

    history page updated

--- src/app/home/history/history.page.ts
+++ src/app/home/history/history.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { StorageService } from '../../services/storage.service';
+import { Ledger } from '../../model/bet-form.model';
 
 @Component({
   selector: 'app-history',
@@ -7,9 +9,24 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./history.page.scss'],
 })
 export class HistoryPage implements OnInit {
-  constructor(private navController: NavController) {}
+  constructor(
+    private navController: NavController,
+    private storage: StorageService
+  ) {}
 
-  ngOnInit() {}
+  betRecords: Ledger[];
+
+  ngOnInit() {
+    this.storage.getAllBets<Ledger>().then((items) => {
+      this.betList(items);
+    });
+  }
+
+  betList(inp: Array<Ledger>): void {
+    this.betRecords = inp.filter(
+      (x) => x.isActive === false && x.result && x.result !== ''
+    );
+  }
 
   goBack() {
     this.navController.navigateRoot('/home');
